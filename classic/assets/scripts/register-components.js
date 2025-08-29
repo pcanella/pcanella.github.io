19e45e81fd197255e0e376e33afa476ad33200f5
Patrick Canella <patrickcanella@gmail.com>
2025-08-29 09:22:16 -0700

    register-component fix for web components

--- classic/assets/scripts/register-components.js
+++
@@ -1,9 +0,0 @@
-// src/scripts/register-components.ts
-import '../../../src/web-components/macintosh-menu/index';
-import '../../../src/web-components/macintosh-menu-item/index';
-import '../../../src/web-components/macintosh-icon-button/index';
-import '../../../src/web-components/macintosh-window/index';
-import '../../../src/web-components/macintosh-button/index';
-import '../../../src/web-components/macintosh-dialog-box/index';
-
-import './interact.js';
---
+++ classicmode-src/src/scripts/register-components.ts
@@ -0,0 +1,9 @@
+// src/scripts/register-components.ts
+import '../web-components/macintosh-menu/index.js';
+import '../web-components/macintosh-menu-item/index.js';
+import '../web-components/macintosh-icon-button/index.js';
+import '../web-components/macintosh-window/index.js';
+import '../web-components/macintosh-button/index.js';
+import '../web-components/macintosh-dialog-box/index.js';
+
+import '../../public/assets/scripts/interact.js';
